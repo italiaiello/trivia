7e4a5fe2a86c3c25f652d0610e7d49772d2cb0be
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-04-08 18:07:48 +1000

    added some emojis

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 import { useDataFetch } from '../../hooks/displayQuestions'
 import Answers from './Answers'
+import QuizFinished from './QuizFinished'
 
 const Quiz = ({ numQuestions, category, difficulty, type }) => {
     
     const [isLoading, questions] = useDataFetch(`https://opentdb.com/api.php?amount=${numQuestions}${category !== 'Any' ? `&category=${category}` : ""}${difficulty !== 'Any' ? `&difficulty=${difficulty}` : ""}${type !== 'Any' ? `&type=${type}` : ""}`)                                   
     const [index, setIndex] = useState(0)
+    const [questionsRemaining, setQuestionsRemaining] = useState(numQuestions)
     const [numCorrectAnswers, setNumCorrectAnswers] = useState(0)
+    const [isQuizFinished, setIsQuizFinished] = useState(false)
 
     const nextQuestion = () => {
         if (index < questions.length - 1) {
             setIndex(index + 1)
+            setQuestionsRemaining(questionsRemaining - 1)
         } else {
-            setIndex(0)
+            setIsQuizFinished(true)
         }
     }
 
@@ -29,22 +33,31 @@ const Quiz = ({ numQuestions, category, difficulty, type }) => {
                isLoading ?
                <h2>Loading Questions</h2>
                :
-                (   
-                    questions.length &&
-                    <article className="questionContainer">
-                        <p>{`Correct Answers: ${numCorrectAnswers}`}</p>
-                        <p>{questions[index].question}</p>
-                        <article className="answers">
-                            <Answers incorrectAnswers={questions[index].incorrect_answers} 
-                                        correctAnswer={questions[index].correct_answer}
-                                        type={questions[index].type}
-                                        nextQuestion={nextQuestion}
-                                        numCorrectAnswers={numCorrectAnswers}
-                                        setNumCorrectAnswers={setNumCorrectAnswers}
-                            />
+               (
+                    isQuizFinished ?
+                    <QuizFinished   numQuestions={numQuestions} 
+                                    numCorrectAnswers={numCorrectAnswers} 
+                    />
+                    :
+                    (   
+                        questions.length &&
+                        <article className="questionContainer">
+                            <p>{`Questions Remaining: ${questionsRemaining}`}</p>
+                            <p>{`Correct Answers: ${numCorrectAnswers}`}</p>
+                            <p>{questions[index].question}</p>
+                            <article className="answers">
+                                <Answers    incorrectAnswers={questions[index].incorrect_answers} 
+                                            correctAnswer={questions[index].correct_answer}
+                                            type={questions[index].type}
+                                            nextQuestion={nextQuestion}
+                                            numCorrectAnswers={numCorrectAnswers}
+                                            setNumCorrectAnswers={setNumCorrectAnswers}
+                                />
+                            </article>
                         </article>
-                    </article>
-                )
+                    )
+               )
+                
            }
        </section>
    )
---
+++ src/components/Quiz/QuizFinished.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Emoji from './Emoji'
+
+const QuizFinished = ({ numQuestions, numCorrectAnswers }) => {
+
+    const score = Math.round((numCorrectAnswers / numQuestions) * 100)
+
+    return (
+        <article>
+            <h3>{`You scored ${numCorrectAnswers}/${numQuestions}`}</h3>
+            {
+                score < 50 ?
+                <p>Better luck next time <Emoji symbol="&#128577;" label="happy smiley" /></p>
+                :
+                (
+                    score >= 50 && score < 70 ?
+                    <p>Congrats, you passed! <Emoji symbol="&#128522;" label="happy smiley" /></p>
+                    :
+                    (
+                        score >= 70 && score < 85 ?
+                        <p>Well done! <Emoji symbol="&#128512;" label="happy smiley" /></p>
+                        :
+                        <p>Wow! You aced it! <Emoji symbol="&#129299;" label="nerd smiley" /></p>
+                    )
+                )
+            }
+        </article>
+    )
+}
+
+export default QuizFinished
