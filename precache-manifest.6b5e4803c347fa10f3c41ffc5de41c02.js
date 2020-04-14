1a2fd83288367e99decf003f099e9637805d1252
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-04-14 18:58:49 +1000

    Updates

---
+++ precache-manifest.6b5e4803c347fa10f3c41ffc5de41c02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b115cdb908fe3f1418d8777a2b233f9",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6ddd97044f0ce6ac15b7",
+    "url": "/react-gh-pages/static/css/main.18b7b804.chunk.css"
+  },
+  {
+    "revision": "5e0af92e8b489ca772c7",
+    "url": "/react-gh-pages/static/js/2.e62f91ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.e62f91ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddd97044f0ce6ac15b7",
+    "url": "/react-gh-pages/static/js/main.72f83f46.chunk.js"
+  },
+  {
+    "revision": "72650e914613af10bf73",
+    "url": "/react-gh-pages/static/js/runtime-main.5bb8f8d6.js"
+  },
+  {
+    "revision": "d89daf440afd33e34919a01494d8e8b5",
+    "url": "/react-gh-pages/static/media/correct.d89daf44.svg"
+  },
+  {
+    "revision": "2afee8d5767e0c502595a0beed227c2b",
+    "url": "/react-gh-pages/static/media/wrong.2afee8d5.svg"
+  }
+]);
