bb5e963335ac45a9d43b94e41ef301e581ed808d
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-04-14 19:12:03 +1000

    Updates

---
+++ precache-manifest.8104fe2f52154840ef3a1d682cddaaa9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8798dae0fcaec33f0809bccca317b42f",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "b2b875b651f8408f31f7",
+    "url": "/trivia/static/css/main.18b7b804.chunk.css"
+  },
+  {
+    "revision": "5e0af92e8b489ca772c7",
+    "url": "/trivia/static/js/2.e62f91ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia/static/js/2.e62f91ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b875b651f8408f31f7",
+    "url": "/trivia/static/js/main.096a05be.chunk.js"
+  },
+  {
+    "revision": "3fe97a4b9b4e25c57f53",
+    "url": "/trivia/static/js/runtime-main.ad58b66b.js"
+  },
+  {
+    "revision": "d89daf440afd33e34919a01494d8e8b5",
+    "url": "/trivia/static/media/correct.d89daf44.svg"
+  },
+  {
+    "revision": "2afee8d5767e0c502595a0beed227c2b",
+    "url": "/trivia/static/media/wrong.2afee8d5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.6b5e4803c347fa10f3c41ffc5de41c02.js"
+  "/trivia/precache-manifest.8104fe2f52154840ef3a1d682cddaaa9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
